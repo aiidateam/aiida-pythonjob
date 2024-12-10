c0bd0cfa0a6900e8ba7716d240232d9808192fa6
Ali <khsrali@gmail.com>
2024-12-10 15:05:48 +0100

    review applied

--- src/aiida_pythonjob/config.py
+++ src/aiida_pythonjob/config.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 from aiida.manage import get_config
 
@@ -6,7 +7,7 @@ from aiida.manage import get_config
 def load_config() -> dict:
     """Load the configuration from the config file."""
     config = get_config()
-    config_file_path = config.dirpath / "pythonjob.json"
+    config_file_path = Path(config.dirpath) / "pythonjob.json"
     try:
         with config_file_path.open("r") as f:
             config = json.load(f)
