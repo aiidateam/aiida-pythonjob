839e3ba2f08c61bac7ec588abcf6025b392d7995
superstar54 <xingwang1991@gmail.com>
2025-01-28 16:32:26 +0100

    Release 0.1.7

--- src/aiida_pythonjob/__init__.py
+++ src/aiida_pythonjob/__init__.py
@@ -1,6 +1,6 @@
 """AiiDA plugin that run Python function on remote computers."""
 
-__version__ = "0.1.6"
+__version__ = "0.1.7"
 
 from .calculations import PythonJob
 from .launch import prepare_pythonjob_inputs
