ed8f50100160419af7cdcff4940a816a026515fa
superstar54 <xingwang1991@gmail.com>
2025-03-23 23:46:53 +0100

    Release 0.2.0

--- src/aiida_pythonjob/__init__.py
+++ src/aiida_pythonjob/__init__.py
@@ -1,6 +1,6 @@
 """AiiDA plugin that run Python function on remote computers."""
 
-__version__ = "0.1.8"
+__version__ = "0.2.0"
 
 from .calculations import PythonJob
 from .decorator import pyfunction
